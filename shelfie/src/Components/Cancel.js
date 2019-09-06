91d574e95aa7fbc27215a4411afd4544d6e81c1a
Madelyn Arsenault <madelynarsenault@Madelyns-MacBook-Air-2.local>
2019-09-06 16:55:19 -0500

    finals

---
+++ shelfie/src/Components/Cancel.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+class Cancel extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+        }
+
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+<button>Cancel</button>
+
+export default Cancel;
+
+
+
+
+
+
+
+
+
+
